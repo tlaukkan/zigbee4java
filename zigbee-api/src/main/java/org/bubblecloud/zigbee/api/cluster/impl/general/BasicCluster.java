b4c5115a21cd809166172705de72bf3fbd6634b4
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2014-01-06 14:11:04 +0200

    Refactored category field to details.

--- zigbee-api/src/main/java/org/bubblecloud/zigbee/api/cluster/impl/general/BasicCluster.java
+++ zigbee-api/src/main/java/org/bubblecloud/zigbee/api/cluster/impl/general/BasicCluster.java
@@ -105,7 +105,7 @@ public class BasicCluster extends ZCLClusterBase implements Basic {
     }
 
     public Attribute getAttributeApplicationVersion() {
-        return alarmMask;
+        return applicationVersion;
     }
 
     public Attribute getAttributeDateCode() {
