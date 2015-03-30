a9cc5a323cb8afe80864c7f3e928a217735f4677
Chris Hatton <christopherhattonuk@gmail.com>
2015-03-30 18:48:55 +1100

    Updated console run-script to correctly versioned filename.

--- zigbee-console-javase/zigbee-console.sh
+++ zigbee-console-javase/zigbee-console.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar target/zigbee-console-javase-2.0.1-SNAPSHOT.jar /dev/cu.usbmodem1411 11 4952 false
+java -jar target/zigbee-console-javase-2.0.3-SNAPSHOT.jar /dev/cu.usbmodem1411 11 4952 false
