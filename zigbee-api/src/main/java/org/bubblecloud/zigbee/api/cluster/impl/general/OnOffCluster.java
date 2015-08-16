aae67047b9972749db86682f8a1e440604dd704b
Chris Jackson <chris@cd-jackson.com>
2015-08-16 13:41:41 +0100

    Update javadoc

--- zigbee-api/src/main/java/org/bubblecloud/zigbee/api/cluster/impl/general/OnOffCluster.java
+++ zigbee-api/src/main/java/org/bubblecloud/zigbee/api/cluster/impl/general/OnOffCluster.java
@@ -35,6 +35,7 @@ import org.bubblecloud.zigbee.api.cluster.impl.core.ZCLClusterBase;
 import org.bubblecloud.zigbee.api.cluster.impl.global.DefaultResponseImpl;
 
 /**
+ * The OnOff Cluster provides attributes and commands for switching devices between ‘On’ and ‘Off’ states. 
  * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano "Kismet" Lenzi</a>
  * @author <a href="mailto:francesco.furfari@isti.cnr.it">Francesco Furfari</a>
  * @version $LastChangedRevision: 799 $ ($LastChangedDate: 2013-08-06 19:00:05 +0300 (Tue, 06 Aug 2013) $)
@@ -105,8 +106,6 @@ public class OnOffCluster extends ZCLClusterBase implements OnOff {
         enableDefaultResponse();
         Response response = invoke(CMD_TOGGLE);
         return new DefaultResponseImpl(response);
-
     }
-
 
 }
