1db90a7fa9bf40984a7de81e6a7feded3199f56b
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-05-29 21:47:17 +0300

    CI configuration.

--- .travis/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ev
-git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
-cd $TRAVIS_REPO_SLUG
-git checkout -qf $TRAVIS_COMMIT
-mvn -B release:prepare release:perform  -DskipTests=true --settings settings.xml
