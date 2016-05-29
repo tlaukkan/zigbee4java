d4fa8928c9fac0842a6c1189098ab83d432c9195
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-05-29 20:56:18 +0300

    CI configuration.

--- scripts/beforedeploy.sh
+++ scripts/beforedeploy.sh
@@ -3,4 +3,3 @@ set -ev
 git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
 cd $TRAVIS_REPO_SLUG
 git checkout -qf $TRAVIS_COMMIT
-mvn -B release:prepare release:perform  -DskipTests=true --settings settings.xml
