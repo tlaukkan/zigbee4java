e662f328eee81cab191389c498776feb69d1df91
Tommi S.E. Laukkanen <tommi.s.e.laukkanen@gmail.com>
2016-05-31 19:59:00 +0300

    Clean up of serial port implementation.

--- zigbee-serial-javase/src/test/java/org/bubblecloud/zigbee/ZigBeeSerialNetworkTest.java
+++ zigbee-serial-javase/src/test/java/org/bubblecloud/zigbee/ZigBeeSerialNetworkTest.java
@@ -6,13 +6,19 @@ import org.bubblecloud.zigbee.network.port.SerialPortImpl;
  * Created by Chris on 25/02/15.
  */
 public final class ZigBeeSerialNetworkTest extends ZigBeeNetworkTest {
-	public static final String SerialPortIdentifier = "/dev/ttyACM0";
-	public static final int    BaudRate             = 115200;
+    /**
+     * The serial port identifier.
+     */
+    public static final String SERIAL_PORT_IDENTIFIER = "/dev/ttyACM0";
+    /**
+     * The baud rate.
+     */
+    public static final int BAUD_RATE = 115200;
 
     /**
      * Constructor for defining the serial port implementation.
      */
-	public ZigBeeSerialNetworkTest() {
-		super(new SerialPortImpl(SerialPortIdentifier, BaudRate));
-	}
+    public ZigBeeSerialNetworkTest() {
+        super(new SerialPortImpl(SERIAL_PORT_IDENTIFIER, BAUD_RATE));
+    }
 }
